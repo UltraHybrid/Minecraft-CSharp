68f12cf3e6da5e8d5b293175bb9af4a0e380d4ea
LobachevDanil <50016131+LobachevDanil@users.noreply.github.com>
2020-03-18 01:00:49 +0500

    world update 2

--- TrialVersion/Point3.cs
+++ TrialVersion/Point3.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using OpenTK;
 
 namespace tmp.TrialVersion
 {
@@ -19,6 +21,16 @@ namespace tmp.TrialVersion
         public int GetDistance(Point3 other)
         {
             return Math.Max(Math.Max(Math.Abs(X - other.X), Math.Abs(Y - other.Y)), Math.Abs(Z - other.Z));
+        }
+
+        public static Point3 operator +(Point3 p1, Point3 p2)
+        {
+            return new Point3(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+        }
+
+        public static implicit operator Vector3(Point3 point)
+        {
+            return new Vector3(point.X, point.Y, point.Z);
         }
     }
 }
--- TrialVersion/World.cs
+++ TrialVersion/World.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using OpenTK;
 
 namespace tmp.TrialVersion
 {
@@ -23,6 +24,54 @@ namespace tmp.TrialVersion
             }
         }
 
+        public Dictionary<string, List<Vector3>> GetVisibleBlock(int x, int z)
+        {
+            var chunk = chunks[x, z];
+            var result = new Dictionary<string, List<Vector3>>();
+            foreach (var block in chunk)
+            {
+                var position = GetAbsolutPosition(block, x, z);
+                if (block.BlockItem == BaseBlocks.Empty || IsBorderOnEmpty(block, position))
+                    continue;
+
+                var key = block.BlockItem.TextureName;
+                if (result.ContainsKey(key))
+                    result[key].Add(position);
+                else
+                    result[key] = new List<Vector3>() {position};
+            }
+
+            return result;
+        }
+
+        private Point3 GetAbsolutPosition(Block block, int x, int z)
+        {
+            return block.Position + new Point3(x * Chunk.XLenght, 0, z * Chunk.ZLength);
+        }
+
+        private bool IsBorderOnEmpty(Block block, Point3 position)
+        {
+            var offsets = new Point3[]
+            {
+                new Point3(-1, 0, 0), new Point3(1, 0, 0),
+                new Point3(0, -1, 0), new Point3(0, 1, 0),
+                new Point3(0, 0, -1), new Point3(0, 0, 1)
+            };
+            for (var i = 0; i < offsets.Length; i++)
+            {
+                if (IsCorrectIndex(position) && this[position + offsets[i]].BlockItem == BaseBlocks.Empty)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool IsCorrectIndex(Point3 position)
+        {
+            return 0 <= position.X && position.X < MaxCount * Chunk.XLenght && 0 <= position.Y &&
+                   position.Y < Chunk.YLength && 0 <= position.Z && position.Z < MaxCount * Chunk.ZLength;
+        }
+
         public IEnumerator<Chunk> GetEnumerator()
         {
             return chunks.Cast<Chunk>().GetEnumerator();
@@ -33,14 +82,14 @@ namespace tmp.TrialVersion
             return GetEnumerator();
         }
 
-        private bool CheckBounds(Point3 position)
+        private bool CheckChunckBounds(Point3 position)
         {
             var numberX = position.X / Chunk.XLenght;
             var numberZ = position.Z / Chunk.ZLength;
-            return CheckBounds(numberX, numberZ);
+            return CheckChunckBounds(numberX, numberZ);
         }
 
-        private bool CheckBounds(int x, int z)
+        private bool CheckChunckBounds(int x, int z)
         {
             return 0 <= x && x < MaxCount && 0 <= z && z < MaxCount;
         }
@@ -49,7 +98,7 @@ namespace tmp.TrialVersion
         {
             get
             {
-                if (!CheckBounds(position))
+                if (!CheckChunckBounds(position))
                     throw new ArgumentException();
                 var numberX = position.X / Chunk.XLenght;
                 var numberZ = position.Z / Chunk.ZLength;
@@ -59,7 +108,7 @@ namespace tmp.TrialVersion
             }
             set
             {
-                if (!CheckBounds(position))
+                if (!CheckChunckBounds(position))
                     throw new ArgumentException();
                 var numberX = position.X / Chunk.XLenght;
                 var numberZ = position.Z / Chunk.ZLength;
@@ -73,13 +122,13 @@ namespace tmp.TrialVersion
         {
             get
             {
-                if (!CheckBounds(x, z))
+                if (!CheckChunckBounds(x, z))
                     throw new ArgumentException();
                 return chunks[x, z];
             }
             set
             {
-                if (!CheckBounds(x, z))
+                if (!CheckChunckBounds(x, z))
                     throw new ArgumentException();
                 chunks[x, z] = value;
             }
