9ca0e87e7c12c7cb23ffd1d82538743a0ce1d769
LobachevDanil <50016131+LobachevDanil@users.noreply.github.com>
2020-03-18 01:39:19 +0500

    new BlockItems

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenTK;
+using tmp.TrialVersion;
 
 namespace tmp
 {
--- TrialVersion/BaseBlocks.cs
+++ TrialVersion/BaseBlocks.cs
@@ -5,7 +5,12 @@ namespace tmp.TrialVersion
     public static class BaseBlocks
     {
         public static readonly BlockItem Empty = new BlockItem("Empty", 0, null);
-        public static readonly BlockItem Dirt = new BlockItem("Dirt", 1, "dirt.png");
+        public static readonly BlockItem Dirt = new BlockItem("Dirt", 3, "dirt.png");
+        public static readonly BlockItem Glass = new BlockItem("Glass", 1, "glass.png");
+        public static readonly BlockItem Stone = new BlockItem("Stone", 10, "stone.png");
+        public static readonly BlockItem Sand = new BlockItem("Sand", 2, "sand,png");
+        public static readonly BlockItem CoalOre = new BlockItem("CoalOre", 5, "coal_ore.png");
+        public static readonly BlockItem Cobblestone = new BlockItem("Cobblestone", 10, "cobblestone.png");
         public static readonly BlockItem Bedrock = new BlockItem("Bedrock", int.MaxValue, "bedrock.png");
     }
 }
