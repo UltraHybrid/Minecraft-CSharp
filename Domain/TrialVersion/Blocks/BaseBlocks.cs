f1f360122f1915dca143fced6f99653977b470b5
danill <danillural@mail.ru>
2020-06-18 01:01:46 +0500

    refactor infrastructure

--- Domain/TrialVersion/Blocks/BaseBlocks.cs
+++ Domain/TrialVersion/Blocks/BaseBlocks.cs
@@ -9,52 +9,82 @@ namespace tmp.Domain.TrialVersion.Blocks
     public static class BaseBlocks
     {
         public static readonly BlockType Dirt =
-            new BlockType("Dirt", 3, TextureInfo.CreateSolid("dirt.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("Dirt", 3,
+                TextureInfo.CreateSolid("dirt.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Glass =
-            new BlockType("Glass", 1, TextureInfo.CreateSolid("glass.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("Glass", 1,
+                TextureInfo.CreateSolid("glass.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Stone =
-            new BlockType("Stone", 10, TextureInfo.CreateSolid("stone.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("Stone", 10,
+                TextureInfo.CreateSolid("stone.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Sand =
-            new BlockType("Sand", 2, TextureInfo.CreateSolid("sand.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("Sand", 2,
+                TextureInfo.CreateSolid("sand.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType CoalOre =
-            new BlockType("CoalOre", 5, TextureInfo.CreateSolid("coal_ore.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("CoalOre", 5,
+                TextureInfo.CreateSolid("coal_ore.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Cobblestone =
-            new BlockType("Cobblestone", 10, TextureInfo.CreateSolid("cobblestone.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("Cobblestone", 10,
+                TextureInfo.CreateSolid("cobblestone.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Bedrock =
-            new BlockType("Bedrock", int.MaxValue, TextureInfo.CreateSolid("bedrock.png", HitBox.Unit),
-                Basis.UnitBasis);
+            new BlockType("Bedrock", int.MaxValue,
+                TextureInfo.CreateSolid("bedrock.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType GrassPath =
             new BlockType("GrassPath", 3,
                 TextureInfo.CreateWithTopAndBottom("grass_path_top.png", "dirt.png", "grass_path_side.png",
                     HitBox.Unit),
-                Basis.UnitBasis);
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Grass =
             new BlockType("Grass", 3,
                 TextureInfo.CreateWithTopAndBottom("grass_top.png", "dirt.png", "grass_block_side.png", HitBox.Unit),
-                Basis.UnitBasis);
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType OakLog =
             new BlockType("OakLog", 4,
                 TextureInfo.CreateWithTopAndBottom("oak_log_top.png", "oak_log_top.png", "oak_log.png", HitBox.Unit),
-                Basis.UnitBasis);
+                Basis.UnitBasis
+            );
 
         public static readonly BlockType Snow =
-            new BlockType("Snow", 2, TextureInfo.CreateSolid("snow.png", HitBox.Unit), Basis.UnitBasis);
+            new BlockType("Snow", 2,
+                TextureInfo.CreateSolid("snow.png", HitBox.Unit),
+                Basis.UnitBasis
+            );
 
         //public static readonly BlockType Fence =
         //    new BlockType("Fence", 3,
         //        TextureInfo.CreateWithTopAndBottom("", "", "", new HitBox(0.25f, 1f, 0.25f, Basis.UnitBasis)),
         //        new Basis(PointF.Zero, 0.25f * Vector3.UnitX, 1.5f * Vector3.UnitY, 0.25f * Vector3.UnitZ));
 
-        public static readonly BlockType Corpuscle = new BlockType("", int.MaxValue, null, Basis.UnitBasis);
+        public static readonly BlockType Corpuscle =
+            new BlockType("", int.MaxValue,
+                null,
+                Basis.UnitBasis
+            );
 
         private static readonly List<BlockType> allBlocks;
 
--- Infrastructure/SimpleMath/SimpleMath.cs
+++ Infrastructure/SimpleMath/SimpleMath.cs
@@ -4,14 +4,19 @@ namespace tmp.Infrastructure.SimpleMath
 {
     public static class SimpleMath
     {
+        public static PointI AsPointI(this PointB point)
+        {
+            return new PointI(point.X, point.Y, point.Z);
+        }
+
         public static PointB AsPointB(this PointI point)
         {
             return new PointB((byte) point.X, (byte) point.Y, (byte) point.Z);
         }
 
-        public static PointI AsPointI(this PointB point)
+        public static PointF AsPointF(this PointI point)
         {
-            return new PointI(point.X, point.Y, point.Z);
+            return new PointF(point.X, point.Y, point.Z);
         }
 
         public static Vector3 AsVector(this PointI point)
@@ -24,16 +29,6 @@ namespace tmp.Infrastructure.SimpleMath
             return new PointF(point.X + vectorLast.X, point.Y + vectorLast.Y, point.Z + vectorLast.Z);
         }
 
-        public static PointI AsPointI(this Vector3 vectorLast)
-        {
-            return new PointI((int) vectorLast.X, (int) vectorLast.Y, (int) vectorLast.Z);
-        }
-
-        public static PointF AsPointF(this PointI point)
-        {
-            return new PointF(point.X, point.Y, point.Z);
-        }
-
         public static PointI AsPointI(this PointF point)
         {
             return new PointI((int) point.X, (int) point.Y, (int) point.Z);
@@ -42,6 +37,11 @@ namespace tmp.Infrastructure.SimpleMath
         public static Vector3 AsVector(this PointF point)
         {
             return new Vector3(point.X, point.Y, point.Z);
+        }
+
+        public static PointI AsPointI(this Vector3 vectorLast)
+        {
+            return new PointI((int) vectorLast.X, (int) vectorLast.Y, (int) vectorLast.Z);
         }
 
         public static PointF AsPointF(this Vector3 vectorLast)
